1ec35b8479e108dd2ca723385b8c51b6198786bc
FURKAN-tas <Prophet.28@hotmail.com>
2022-01-27 22:18:58 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const addForm=document.querySelector('.add');
+const list=document.querySelector('.todos')
+const search=document.querySelector('.search input');
+
+const generateTemplate = todo =>{
+    const html=
+            `<li class="list-group-item d-flex justify-content-between align-items-center">
+             <span>${todo}</span>
+             <i class="far fa-trash-alt delete"></i>
+            </li>`;
+         list.innerHTML+=html;
+}
+
+addForm.addEventListener('submit',e =>{
+    e.preventDefault();
+    const todo=addForm.add.value.trimStart();
+    //console.log(todo);
+    if(todo.length > 0)
+    {
+        generateTemplate(todo);
+        addForm.reset();
+    }
+})
+
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete'))
+    {
+        e.target.parentElement.remove();
+    }
+})
+
+const filterTodos = term =>{
+ 
+    Array.from(list.children)
+    .filter(todo => !todo.textContent.includes(term))
+    .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+    .filter(todo => todo.textContent.includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+}
+
+search.addEventListener('keyup', () => {
+    const term=search.value.trim();
+    filterTodos(term);
+})
